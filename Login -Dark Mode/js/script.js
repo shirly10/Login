64616abe0b6fe3a63c660c10ec7fda0c3102c323
shirly10 <shirlynieves04@gmail.com>
2024-08-20 12:19:43 -0500

    Add files via upload

---
+++ Login -Dark Mode/js/script.js
@@ -0,0 +1,22 @@
+function mostrarNombre(){
+    let Nombre = document.getElementById("campoNombre").value;
+    let Apellido = document.getElementById("campoApellido").value;
+    let User = document.getElementById("campoUser").value;
+    let Email = document.getElementById("campoEmail").value;
+    let Contraseña = document.getElementById("campoContraseña").value;
+    // let Mensaje = ["Nombre: "+ campoNombre + "\n"+ "Apellido: "+ campoApellido + "\n" + "User: " + campoUser + "\n" + "Email: " + campoEmail + "\n" + "Contraseña :" + campoContraseña]
+
+    alert(` informacion ingresada: \n
+        Nombre: ${Nombre} \n
+        Apellido: ${Apellido} \n
+        User: ${User} \n
+        Email: ${Email} \n
+        Contraseña: ${Contraseña} \n
+    `);
+
+}
+
+
+function activarClase(){
+document.body.classList.toggle("black");
+}
